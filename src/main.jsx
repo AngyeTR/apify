0d0fb40642405480141da7496011e31d33cfe983
Angie <angie.taroga@gmail.com>
2025-06-03 14:55:14 -0500

    fx: changed some responsive items

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import { BrowserRouter as Router} from "react-router-dom"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GridStack } from 'gridstack';
+import 'gridstack/dist/gridstack.min.css';
+
+// En tu componente o lugar donde lo necesites
+useEffect(() => {
+  const grid = GridStack.init();
+  // Aquí puedes usar grid como necesites
+}, []);
 
 createRoot(document.getElementById('root')).render(
   <Router>
