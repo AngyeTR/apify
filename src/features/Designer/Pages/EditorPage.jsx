b002a63e47e440c3d8e57823d7cf3147b60c57eb
Angie <angie.taroga@gmail.com>
2025-06-05 11:54:48 -0500

    fx: Resolving designer issues

--- src/features/Designer/Pages/EditorPage.jsx
+++ src/features/Designer/Pages/EditorPage.jsx
@@ -31,8 +31,10 @@ export const EditorPage=()=>{
         savedColor && setLayoutColor(JSON.parse(savedColor))
     }, [layouts ]);
     return(
+        // <div className=" h-screen w-screen sm:grid sm:grid-cols-7 gap-1 justify-items-center m-1 sm:m-5 p-0 mt-5 ">
         <div className=" h-screen w-screen  justify-items-center m-1 sm:m-5 p-0 mt-5 ">
-            <div className="w-[95vw] mb-4 sm:w-[10vw]  m-0 p-0 ">
+            <div className="w-[95vw] mb-4  m-0 p-0 ">
+            {/* <div className="w-[95vw] mb-4 sm:w-[10vw]  m-0 p-0 "> */}
             {/* <div className="w-[95vw] mb-4 sm:w-[15vw]  m-0 p-0 "> */}
                 <WidgetsContainer  setItems={setLayout} items={layout} count={count} setCount={setCount} layoutColor={layoutColor} item={item} grid={grid}/>
             </div>
