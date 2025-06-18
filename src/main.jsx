9020c0683ee236de57f05336f35b80cc3251013f
Angie <angie.taroga@gmail.com>
2025-06-18 10:05:20 -0500

    FT Added defaultValues in Tunnel Wizard

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,7 @@ import { BrowserRouter as Router} from "react-router-dom"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { GridStack } from 'gridstack';
 import 'gridstack/dist/gridstack.min.css';
-import { useEffect } from "react";
-
-// useEffect(() => {
-//   const grid = GridStack.init();
-// }, []);
 
 createRoot(document.getElementById('root')).render(
   <Router>
