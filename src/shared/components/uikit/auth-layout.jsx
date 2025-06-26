6ad9b8c392c5221fb93e3f384dfd8557cb6664e5
Angie <angie.taroga@gmail.com>
2025-06-26 16:57:51 -0500

    FT adding chat Feature

--- src/shared/components/uikit/auth-layout.jsx
+++ src/shared/components/uikit/auth-layout.jsx
@@ -1,7 +1,7 @@
 export function AuthLayout({ children }) {
   return (
     <main className="flex min-h-dvh flex-col p-2">
-      <div className="flex grow items-center justify-center p-6 lg:rounded-lg lg:bg-white lg:p-10 lg:shadow-xs lg:ring-1 lg:ring-zinc-950/5    ">
+      <div className="flex grow items-center justify-center p-6 lg:rounded-lg lg:bg-neutral-50 lg:p-10 lg:shadow-xs lg:ring-1 lg:ring-zinc-950/5    ">
         {children}
       </div>
     </main>
